dc1e0f9730f7e597efb9e409bcc3bd5bd1850e27
Abdullah All Kafi <kafi2006jk@gmail.com>
2025-02-12 16:27:46 +0600

    update Ui finally

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -20,7 +20,6 @@ const Footer = () => {
                         <ul className="mt-4 space-y-2">
                             <li><a href="/" className="hover:text-white">Home</a></li>
                             <li><a href="/marathons" className="hover:text-white">All Marathons</a></li>
-                            <li><a href="/add-marathon" className="hover:text-white">Add Marathon</a></li>
                             <li><a href="/dashboard/my-marathons" className="hover:text-white">My Marathons</a></li>
                             <li><a href="/dashboard/my-applications" className="hover:text-white">My Applications</a></li>
                         </ul>
@@ -30,7 +29,7 @@ const Footer = () => {
                     <div>
                         <h3 className="text-xl font-bold text-white border-b-2 border-red-500 inline-block pb-2">Contact Us</h3>
                         <ul className="mt-4 space-y-2 text-gray-400">
-                            <li>Email: <a href="mailto:support@racerosterpro.com" className="hover:text-white">support@marathon-x.com</a></li>
+                            <li>Email: <a href="mailto:support@marathonx@gmail.com" className="hover:text-white">support@marathon-x.com</a></li>
                             <li>Phone: <a href="tel:+1234567890" className="hover:text-white">+1 (234) 567-890</a></li>
                             <li>Address: 456 Marathon Way, Event City</li>
                         </ul>
@@ -40,12 +39,7 @@ const Footer = () => {
                 {/* Bottom Footer Section */}
                 <div className="mt-8 border-t border-gray-700 pt-4 text-center">
                     <p>&copy; {new Date().getFullYear()} Marathon-X. All rights reserved.</p>
-                    <p className="mt-2">
-                        Follow us: 
-                        <a href="https://facebook.com" className="ml-2 text-blue-500 hover:text-white">Facebook</a> | 
-                        <a href="https://twitter.com" className="ml-2 text-cyan-500 hover:text-white">Twitter</a> | 
-                        <a href="https://instagram.com" className="ml-2 text-pink-500 hover:text-white">Instagram</a>
-                    </p>
+                   
                 </div>
             </footer>
         </>
